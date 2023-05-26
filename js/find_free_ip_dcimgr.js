3b59976c9cbb6526835632d71371fbc74d29fbfb
thirdmadman <thirdmadman@gmail.com>
2023-05-26 14:49:54 +0200

    feat: update findFreeIp

--- js/find_free_ip_dcimgr.js
+++ js/find_free_ip_dcimgr.js
@@ -1,43 +1,33 @@
 async function findFreeIp(netId) {
-  let url =
-      "https://ds.smartape.net/api/ip/v3/ip?orderby=name+asc&limit=0,300&where=ipnet+EQ+"+netId+"+AND+status+NE+%27extended%27";
-  let response = await fetch(url, {
-      headers: {
-          "isp-box-instance": "true",
-      },
-  });
+  const url = `https://ds.smartape.net/api/dci/v3/ipmgr/ip?orderby=name%20asc&limit=0,512&where=NOT%20domain%20CP%20%27%25extended.%25%27+AND+(ipnet%20EQ%20${netId})`;
 
-  let json = {};
+  const response = await fetch(url, { headers: { 'isp-box-instance': 'true' } });
 
-  if (response.ok) {
-      json = await response.json();
-  } else {
-      console.log("Ошибка HTTP: " + response.status);
+  if (!response.ok) {
+    console.error(`HTTP Error when calling API: ${response.status}`);
+    return [];
   }
 
-  function netMasks(number) {
-      return Math.pow(2, (32-number));
+  const json = await response.json();
+
+  const getNetworkMasks = (number) => 2 ** (32 - number);
+
+  const subnetMask = json.list[0].ipnet_name.split('/')[1];
+  const subnetAddress = json.list[0].ipnet_name.split('/')[0];
+
+  const sortedList = json.list.sort((a, b) => parseInt(a.ip_addr.split('.')[3], 10) - parseInt(b.ip_addr.split('.')[3], 10));
+
+  const networkName = subnetAddress.split('.').slice(0, 3).join('.');
+  const networkNameNumber = subnetAddress.split('.').slice(3, 4);
+
+  const startNum = parseInt(networkNameNumber, 10) + 1;
+  const endNum = parseInt(networkNameNumber, 10) + parseInt(getNetworkMasks(parseInt(subnetMask, 10)), 10);
+
+  const freeIp = [];
+  for (let i = startNum; i < endNum; i += 1) {
+    freeIp.push(`${networkName}.${i}`);
   }
-
-  let subnetMask = json.list[0].ipnet_name.split("/")[1];
-  let subnetAdress = json.list[0].ipnet_name.split("/")[0];
-
-  let sortedlist = json.list.sort(
-      (a, b) =>
-          parseInt(a.name.split(".")[3]) - parseInt(b.name.split(".")[3])
-  );
-  
-  let netname = subnetAdress.split(".").slice(0, 3).join(".");
-  let netnameNum = subnetAdress.split(".").slice(3, 4);
-
-  let startNum = parseInt(netnameNum) + 1;
-  let endNum = parseInt(netnameNum) + parseInt(netMasks(parseInt(subnetMask)));
-
-  let freeIp = [];
-  for (let i = startNum; i < endNum; i++) {
-      freeIp.push(netname + "." + i);
-  }
-  return freeIp.filter(el => !(sortedlist.map(el2 => el2.name).includes(el)));
-
+  return freeIp.filter((el) => !sortedList.map((el2) => el2.ip_addr).includes(el));
 }
-findFreeIp(window.location.pathname.split("/").slice(4,5)).then(result => console.log(result));
+
+findFreeIp(window.location.pathname.split('/').slice(4, 5)).then((result) => console.log(result));
